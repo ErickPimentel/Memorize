a1d05242f00eb03c63fbe694dacdbf470fd93b4a
Erick Spinelli Pimentel <erickspinellipimentel@gmail.com>
2022-10-10 22:05:52 -0300

    Lecture 5: ViewBuilder + Shape + ViewModifier

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -10,7 +10,7 @@ import SwiftUI
 class EmojiMemoryGame: ObservableObject {
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
-    static func createMemoryGame() -> MemoryGame<String> {
+    private static func createMemoryGame() -> MemoryGame<String> {
         let emojis = ["👻","🎃", "🕷"]
         return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
             return emojis[pairIndex]
@@ -19,7 +19,7 @@ class EmojiMemoryGame: ObservableObject {
     
     //MARK: - Access to the Model
     var cards: Array<MemoryGame<String>.Card>{
-        return model.cards
+        model.cards
     }
     
     // MARK: - Intent(s)
